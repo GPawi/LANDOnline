8ca239eb0977ed0800b22d5574c45812b55b7d88
GPawi <Gregor.Pfalz@awi.de>
2023-03-11 23:51:19 +0000

    rstan try 6
    I know that it is not good practice to publish everything in the main, but no one is using it, so I use it for now to test it... if it is public, testing will be a separate branch, don't worry!

--- install.R
+++ install.R
@@ -1,7 +1,21 @@
 install.packages("Bchron", repos = "https://cloud.r-project.org")
 library("devtools")
+#New test - from binder-rethinking
 Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1)
-#install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, "Makevars")
+if (!file.exists(M)) file.create(M)
+cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+    "CXX14=g++", # or clang++ but you may need a version postfix
+    file = M, sep = "\n", append = TRUE)
+dotR <- file.path(Sys.getenv("HOME"), ".R")
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, "Makevars")
+if (!file.exists(M)) file.create(M)
+cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+    "CXX14=g++", # or clang++ but you may need a version postfix
+    file = M, sep = "\n", append = TRUE)
+install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 install_github("edwindj/ffbase", subdir="pkg")
 install_github("earthsystemdiagnostics/hamstr", ref="master")
 install_github("earthsystemdiagnostics/hamstrbacon", ref="main")
