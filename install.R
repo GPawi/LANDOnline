9b03587ab8663ba135a87d545b17defe76e0f9c3
GPawi <Gregor.Pfalz@awi.de>
2023-03-12 00:10:41 +0000

    rstan try 7

--- install.R
+++ install.R
@@ -1,23 +1,24 @@
-install.packages("Bchron", repos = "https://cloud.r-project.org")
-library("devtools")
 #New test - from binder-rethinking
 Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1)
-if (!file.exists(dotR)) dir.create(dotR)
-M <- file.path(dotR, "Makevars")
-if (!file.exists(M)) file.create(M)
-cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
-    "CXX14=g++", # or clang++ but you may need a version postfix
-    file = M, sep = "\n", append = TRUE)
-dotR <- file.path(Sys.getenv("HOME"), ".R")
-if (!file.exists(dotR)) dir.create(dotR)
-M <- file.path(dotR, "Makevars")
-if (!file.exists(M)) file.create(M)
-cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
-    "CXX14=g++", # or clang++ but you may need a version postfix
-    file = M, sep = "\n", append = TRUE)
+#if (!file.exists(dotR)) dir.create(dotR)
+#M <- file.path(dotR, "Makevars")
+#if (!file.exists(M)) file.create(M)
+#cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+#    "CXX14=g++", # or clang++ but you may need a version postfix
+#    file = M, sep = "\n", append = TRUE)
+#dotR <- file.path(Sys.getenv("HOME"), ".R")
+#if (!file.exists(dotR)) dir.create(dotR)
+#M <- file.path(dotR, "Makevars")
+#if (!file.exists(M)) file.create(M)
+#cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+#    "CXX14=g++", # or clang++ but you may need a version postfix
+#    file = M, sep = "\n", append = TRUE)
 install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
-install_github("edwindj/ffbase", subdir="pkg")
+library("devtools")
+library("rstan")
 install_github("earthsystemdiagnostics/hamstr", ref="master")
+install.packages("Bchron", repos = "https://cloud.r-project.org")
+install_github("edwindj/ffbase", subdir="pkg")
 install_github("earthsystemdiagnostics/hamstrbacon", ref="main")
 install_version("clam", version="2.3.9", repos= "https://cloud.r-project.org")
 install_version("rbacon", version="2.5.2", repos= "https://cloud.r-project.org")
