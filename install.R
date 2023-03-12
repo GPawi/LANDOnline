791484abb0300a56c45d63fc3ff3ad8cba020a8c
GPawi <Gregor.Pfalz@awi.de>
2023-03-12 01:13:24 +0000

    Test for octave
    Everything worked well with R, so last thing is to get the packages for octave installed

--- install.R
+++ install.R
@@ -1,19 +1,3 @@
-#New test - from binder-rethinking
-#Sys.setenv(DOWNLOAD_STATIC_LIBV8 = 1)
-#if (!file.exists(dotR)) dir.create(dotR)
-#M <- file.path(dotR, "Makevars")
-#if (!file.exists(M)) file.create(M)
-#cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
-#    "CXX14=g++", # or clang++ but you may need a version postfix
-#    file = M, sep = "\n", append = TRUE)
-#dotR <- file.path(Sys.getenv("HOME"), ".R")
-#if (!file.exists(dotR)) dir.create(dotR)
-#M <- file.path(dotR, "Makevars")
-#if (!file.exists(M)) file.create(M)
-#cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
-#    "CXX14=g++", # or clang++ but you may need a version postfix
-#    file = M, sep = "\n", append = TRUE)
-#install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
 library("devtools")
 options(unzip = "internal")
 install_github("earthsystemdiagnostics/hamstr", ref="master")
